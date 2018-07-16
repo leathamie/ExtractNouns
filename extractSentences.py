90b4fa5254d24c70471d3431983d76f3137f23ee
leathamie <lea.thamie@outlook.fr>
2018-07-16 09:21:01 -0700

    funct for get speech lines and get all part of the sentence

---
+++ extractSentences.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 16 07:59:39 2018
+
+@author: lea
+"""
+
+import re
+
+def extractFileContent(filename):
+    fileContent = ""
+    file = open(filename, "r")
+    for line in file : 
+        fileContent = fileContent + line
+    return fileContent
+
+
+def getSpeechLines(filename):
+    content = extractFileContent(filename)
+    line_tab = re.findall('\*MOT:.*\n', content)
+    print (len(line_tab))
+    return line_tab
+def splitLines(SpeechLines):
+    line = SpeechLines[0]
+    line = line.split(':\t')
+    act = line[0]
+    act = act.replace('*','')
+    print(act)
+    line = line[1]
+    line = line.split('\x15')
+    sentence = line[0]
+    line = line[1]
+    line = line.split('_')
+    on = line[0]
+    off = line[1]
+    print ("sentence "+sentence)
+    print ("on " + on)
+    print ("off "+ off)
+    
+    
+        
+def getAllSpeechDuration(filename):
+    content = extractFileContent(filename)
+    duration_tab = re.findall('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
+    return duration_tab
+            
+def getBigining(filename):
+    content = extractFileContent(filename)
+    m = re.search('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
+    first_duration = m.group(0)
+    on = first_duration.split('_')[0]
+    return on
+
+def getEnd(filename):
+    content = extractFileContent(filename)
+    last_duration = re.findall("[0123456789][0123456789]*_[0123456789][0123456789]*", content)[-1]
+    off = last_duration.split('_')[1]
+    print(off)
+    return off
+
+
+lines = getSpeechLines('/home/lea/Stage/DATA/chaFiles/Rollins/nb09.cha')
+print ("resultat de getSpeechLines" + str(lines))
+splitLines(lines)
+
