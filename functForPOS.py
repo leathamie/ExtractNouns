9ad7a160f948c6a8c51debc13f2eb114d86522e7
leathamie <lea.thamie@outlook.fr>
2018-07-23 09:36:48 -0700

    tests on wordNet

--- functForPOS.py
+++ functForPOS.py
@@ -7,8 +7,24 @@ Created on Tue Jul 17 08:19:04 2018
 """
 import nltk
 import nltk.tokenize
-sentence1 = "here can we put the green one on ."
+from nltk.corpus import wordnet as wn
+
+word1 = "bigbird birdie cow piggy bird box baby book bear glasses kitty bunny firetruck hat stuff pie sheep boat piggie rings babys rug music orange blue police"
 sentence2 = "let's see if we can stack the [/] the rings ."
 
-text = nltk.tokenize.word_tokenize(sentence2)
-print (nltk.pos_tag(text))
+
+toy = wn.synsets('toy')
+print (toy[0].definition())
+obj = wn.synsets('object')
+print (obj[0].definition())
+print(str(toy[0].path_similarity(obj[0])))
+word1= word1.split(' ')
+
+for elt in word1:
+    syns = wn.synsets(elt)
+    if len(syns)>0:
+        print(elt )
+        print("similarité avec objet " + str(syns[0].path_similarity(obj[0])))
+        print("similarité avec toy " + str(syns[0].path_similarity(toy[0])))
+
+
