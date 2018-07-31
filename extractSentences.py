e45c23d12eafaa82dc278ea1a34aba7c1dc1c0de
leathamie <lea.thamie@outlook.fr>
2018-07-31 05:49:03 -0700

    add comment

--- extractSentences.py
+++ extractSentences.py
@@ -11,10 +11,12 @@ import os
 import nltk
 import nltk.tokenize
 import argparse
-from nltk import pos_tag, word_tokenize
-from nltk.corpus import wordnet as wn 
+from nltk import word_tokenize
 
 def extractFileContent(filename):
+    """
+        take a cha file path and return a str with cha content
+    """
     fileContent = ""
     file = open(filename, "r")
     for line in file : 
@@ -23,12 +25,20 @@ def extractFileContent(filename):
 
 
 def getSpeechLines(filePath):
+    """
+        take a chafile path return a tab with only speech lines
+    """
     content = extractFileContent(filePath)
+    # regular expression for speech lines
     line_tab = re.findall('\*[A-Z]+:.*_[0123456789][0123456789]*', content)
-    #print (len(line_tab))
     return line_tab
 
 def splitLines(speechLines):
+    """
+        take a tab with all speech lines of a file in str, return a tab with 
+        all speech lines in a tab : [Speaker, SpeechStart, SpeechEnd, Speech]
+        And the all speech is cleaned
+    """
     lines = []
     for line in speechLines:
         splitLine = splitOneLine(line)
@@ -38,43 +48,48 @@ def splitLines(speechLines):
     return lines
 
 def splitOneLine(line):
-    #print (line)
+    """
+        take a speech line (str) and return a tab with :
+        speaker, speechStart, SpeechEnd, speechCleaned
+    """
     line = line.split(':\t')
     act = line[0]
     act = act.replace('*','')
-    #print(act)
     line = line[1]
     line = line.split('\x15')
     sentence = line[0]
-    sentences = cleanSentence(sentence)
+    sentences = cleanSentence(sentence)#Clean Speech; can become empty( ex: sentence = "[-]" cleanedSentence = "")
     lines = []
     line = line[1]
     line = line.split('_')
     on = line[0]
     off = line[1]
     for sentence in sentences:
-        if sentence != "" and sentence != " " :
-            #print ("sentence "+sentence)
-            #print ("on " + on)
-            #print ("off "+ off)
+        if sentence != "" and sentence != " " : #if cleaned sentence not empty
             lines.append([act, on, off, sentence])
-    return lines
+    return lines # return tab line, can be an empty tab if cleaned sentence is empty
 
 
 def cleanSentence(sentence):
+    """
+        clean a cha sentence, return only the speech or empty str if there is no speech
+    """
     sentence = sentence.replace("[/]","\n")
     sentences = sentence.split("\n")
-    cleanSentences = []
+    cleanedSentences = []
     for sentence in sentences:
         sentence = re.sub('\[.*\]','',sentence)
         sentence = re.sub("[^a-zA-Z ']",'',sentence)
         sentence = sentence.lower()
         sentence = re.sub('xxx','',sentence)
         sentence = re.sub('  +',' ',sentence)
-        cleanSentences.append(sentence)
-    return cleanSentences
+        cleanedSentences.append(sentence)
+    return cleanedSentences
 
 def getFileName(filePath):
+    """
+        take a file path and return only the file name without the extension
+    """
     filename = filePath.split('/')[-1]
     if len(filename) == 0 :
         filename =filePath.split('.')[0]
@@ -82,40 +97,35 @@ def getFileName(filePath):
         filename = filename.split('.')[0]
     return filename
     
-def saveInFile(filePath):
-    lines = getSpeechLines(filePath)
-    print (lines)
-    contentTab = splitLines(lines)
-    filename = getFileName(filePath)
-    file = open("DATA/" + filename + '.txt','w')
-    file.write('name,on,off,sentence,Objects\n')
-    #print ("len de contentTab" + str(len(contentTab)))
-    for content in contentTab:
-        content = add_obj_POS_sentence(content)
-        content = tabToString(content)
-        file.write(content)
             
 def writeFileWithAllObject(chaFilePath):
-    text = getTextFromCha(chaFilePath)
-    objFound = get_obj_by_POS_and_sentences(text)
+    """
+        take a cha file a create a txt file with clean text and obj list
+        like :  speakerName, speechStart,speechEnd, Speech, objectsFound
+    """
+    text = getTextFromCha(chaFilePath) # get a str with the speech only
+    objFound = get_obj_by_POS_and_sentences(text) # get a list of objects found in the speech
     filename = getFileName(chaFilePath)
     file = open("DATA/" + filename + '.txt','w')
-    lines = getSpeechLines(chaFilePath)
-    contentTab = splitLines(lines)
+    lines = getSpeechLines(chaFilePath) # get a tab with speech lines 
+    contentTab = splitLines(lines) # split the lines to a tab with speaker, speechStart, speechEnd, speech 
     file.write('name,on,off,sentence,Objects\n')
     for content in contentTab: #Browse the speech lines 
-        objList = ""
+        objList = "" # str for obj found in a sentence
         for elt in objFound: # Browe all the found objects in the speech
             if " " + elt in content[3]: #if the object is in the sentence. Without the " " before elt, the elt "hat" is found in "what" 
                 if objList != "": 
                     objList += " "
                 objList += elt
         content.append(objList)
-        content = tabToString(content)
-        file.write(content)
+        content = tabToString(content) # convert a tab to a str separated by ","
+        file.write(content) # write the line to the txt file
     
 
 def tabToString(lineTab):
+    """
+        Take a tab and return a str with all the table cells separated by ','
+    """
     line = ""
     for i in range (0,len(lineTab)):
         if len(lineTab)-1 == i:
@@ -125,6 +135,103 @@ def tabToString(lineTab):
     return line 
         
 
+
+def getNouns(sentence):
+    """
+        Take a sentence (str), retrurn the nouns singular or plural using 
+        NTLK POS tag
+    """
+    nouns = ""
+    sentence = word_tokenize(sentence)
+    pos_sentence = nltk.pos_tag(sentence)
+    for elt in pos_sentence:
+        # NN for singular nouns and NNS for plural nouns
+        if elt[1] == 'NN' or elt[1] == 'NNS':
+            if nouns != "":
+                nouns = nouns + " "
+            nouns = nouns + elt[0]
+    return nouns
+
+
+def getObj(sentence):
+    """
+        take  a sentence and return a tab with words who appear 
+        in a specific place in tipical sentences
+    """
+    obj = ""
+    #List of words or phrases that can be preceded by an object name 
+    matchSentences = ["can you get the ", "the ", "da ","that is a ","that's a ","and the ","a ","it is a ","this is a ","and a ","can you say ","here is the ","and ","where is the ","that is the ","look at the ","i have the ","you want the ","color is the ","is that the ","there is the ","you put the ","to put the ","one is the "]
+    for matchSentence in matchSentences:
+        matchResults = re.findall(' ' +matchSentence + '[a-z]+',sentence) #find word groups that are: sample sentence + word
+        for match in matchResults:
+            match = match.replace(' ' + matchSentence,'') #remove sample sentence
+            if obj != "": # if it's not the firsh object found
+                if len(re.findall(match,obj)) == 0: # if the object has not already been found
+                    obj = obj + " "
+                    obj = obj + match 
+            else :
+                obj = obj + match 
+    return obj
+
+def get_obj_by_POS_and_sentences(sentence):
+    """
+        take a str and retrun a list of objects, 
+        the list is the intersection of strategy 
+        1- obj found by typical sentences
+        2- nouns found by POS tagging
+    """
+    list_obj1 = getObj(sentence) 
+    list_obj2 = getNouns(sentence)
+    list_obj1 = list_obj1.split(" ")
+    list_obj2 = list_obj2.split(" ")
+    return list(set(list_obj1).intersection(list_obj2))
+
+
+
+def getTextFromCha(filePath):
+    """
+        take cha file return a str with only speech cleaned
+    """
+    lines = getSpeechLines(filePath)  #tab with speech lines only
+    txt = ""
+    for line in lines: 
+        if txt != "":
+            txt = txt + ". "
+        splittedLine = splitOneLine(line)
+        if len (splittedLine) >0:# if the splitted line is not empty --> the speech is not empty
+            txt = txt + splittedLine[0][3]
+    txt = txt.replace(" .",".")
+    txt = re.sub("\.+",".", txt)
+    return (txt) 
+    
+
+
+def main():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", help=".cha file folder path")
+    args = vars(ap.parse_args())
+    if args.get("input", None) is not None:
+        # read arguments
+        chaFolderPath = args["input"]
+        for filename in os.listdir(chaFolderPath):
+            """
+                Browe all the cha files, for each files in the folder check 
+                if it's a cha file and if it's a Rollins cha file like 
+                if true : find objects in speech and write it in a .txt file
+            """
+            match = re.match("[a-z][a-z][0-9][0-9]\.cha",filename)
+            if match:
+                if chaFolderPath[-1] != "/":
+                    writeFileWithAllObject(chaFolderPath + "/" + filename)
+                else:
+                    writeFileWithAllObject(chaFolderPath + filename)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
 def addNouns(line):
     nouns = getNouns(line[3])
     line.append(nouns)
@@ -146,43 +253,46 @@ def add_obj_POS_sentence(line):
         str_obj += elt
     line.append(str_obj)
     return line
+def set_ratio_each_obj_foud_notFoud(text, objDic):
+    for obj in objDic:
+        nb  = len (re.findall(obj['object'], text))
+        nb = float(obj['nbfound']/nb)
+        obj['ratio'] = nb
+    return objDic
+        
+def cleanObjList(objList):
+    dicList = []
+    for i in range (0, len(objList)):
+        obj = objList[i]
+        n = 0
+        for j in range(0, len(objList)):
+            if obj == objList[j]:
+                if j < i:
+                    break
+                else:
+                    n = n + 1
+        if n > 0:
+            dicList.append({'object' : obj, 'nbfound' : n, 'ratio': 0})
+    return dicList
+          
+def getAllSpeechDuration(filename):
+    content = extractFileContent(filename)
+    duration_tab = re.findall('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
+    return duration_tab
+            
+def getBigining(filename):
+    content = extractFileContent(filename)
+    m = re.search('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
+    first_duration = m.group(0)
+    on = first_duration.split('_')[0]
+    return on
 
-def getNouns(sentence):
-    nouns = ""
-    sentence = word_tokenize(sentence)
-    pos_sentence = nltk.pos_tag(sentence)
-    for elt in pos_sentence:
-        if elt[1] == 'NN' or elt[1] == 'NNS':
-            if nouns != "":
-                nouns = nouns + " "
-            nouns = nouns + elt[0]
-    return nouns
+def getEnd(filename):
+    content = extractFileContent(filename)
+    last_duration = re.findall("[0123456789][0123456789]*_[0123456789][0123456789]*", content)[-1]
+    off = last_duration.split('_')[1]
+    return off
 
-
-def getObj(sentence):
-    obj = ""
-    matchSentences = ["can you get the ", "the ", "da ","that is a ","that's a ","and the ","a ","it is a ","this is a ","and a ","can you say ","here is the ","and ","where is the ","that is the ","look at the ","i have the ","you want the ","color is the ","is that the ","there is the ","you put the ","to put the ","one is the "]
-    for matchSentence in matchSentences:
-        matchResults = re.findall(' ' +matchSentence + '[a-z]+',sentence)
-        for match in matchResults:
-            match = match.replace(' ' + matchSentence,'')
-            if obj != "":
-                if len(re.findall(match,obj)) == 0:
-                    obj = obj + " "
-                    obj = obj + match 
-            else :
-                obj = obj + match 
-    return obj
-
-def get_obj_by_POS_and_sentences(sentence):
-    list_obj1 = getObj(sentence)
-    list_obj2 = getNouns(sentence)
-    list_obj1 = list_obj1.split(" ")
-    list_obj2 = list_obj2.split(" ")
-    return list(set(list_obj1).intersection(list_obj2))
-    
-
-################################  
 def ratioObjectsMatch(filePath):
     text = getTextFromCha(filePath)
     objList = getObj(text).split(" ")
@@ -208,7 +318,7 @@ def perfObjMatch(arg):
         cat = "Union NLTK POS tagging and sentences"
     else : 
         objFound = getObj(text).split(" ")
-        cat = "Typical sentences "
+        cat = "Sample sentences "
     
     objFound = cleanObjList(objFound)
     objFound = set_ratio_each_obj_foud_notFoud(text, objFound)
@@ -238,87 +348,16 @@ def getTotalRatio(objDic):
     print (ratio)
     return ratio
 
-def getTextFromCha(filePath):
+def saveInFile(filePath):
     lines = getSpeechLines(filePath)
-    txt = ""
-    for line in lines:
-        if txt != "":
-            txt = txt + ". "
-        splittedLine = splitOneLine(line)
-        if len (splittedLine) >0:
-            txt = txt + splittedLine[0][3]
-    txt = txt.replace(" .",".")
-    txt = re.sub("\.+",".", txt)
-    return (txt)
-    
-def set_ratio_each_obj_foud_notFoud(text, objDic):
-    for obj in objDic:
-        nb  = len (re.findall(obj['object'], text))
-        nb = float(obj['nbfound']/nb)
-        obj['ratio'] = nb
-    return objDic
+    print (lines)
+    contentTab = splitLines(lines)
+    filename = getFileName(filePath)
+    file = open("DATA/" + filename + '.txt','w')
+    file.write('name,on,off,sentence,Objects\n')
+    #print ("len de contentTab" + str(len(contentTab)))
+    for content in contentTab:
+        content = add_obj_POS_sentence(content)
+        content = tabToString(content)
+        file.write(content)
         
-def cleanObjList(objList):
-    dicList = []
-    for i in range (0, len(objList)):
-        obj = objList[i]
-        n = 0
-        for j in range(0, len(objList)):
-            if obj == objList[j]:
-                if j < i:
-                    break
-                else:
-                    n = n + 1
-        if n > 0:
-            dicList.append({'object' : obj, 'nbfound' : n, 'ratio': 0})
-    return dicList
-    
-    
-
-################################       
-def getAllSpeechDuration(filename):
-    content = extractFileContent(filename)
-    duration_tab = re.findall('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
-    return duration_tab
-            
-def getBigining(filename):
-    content = extractFileContent(filename)
-    m = re.search('[0123456789][0123456789]*_[0123456789][0123456789]*', content)
-    first_duration = m.group(0)
-    on = first_duration.split('_')[0]
-    return on
-
-def getEnd(filename):
-    content = extractFileContent(filename)
-    last_duration = re.findall("[0123456789][0123456789]*_[0123456789][0123456789]*", content)[-1]
-    off = last_duration.split('_')[1]
-    return off
-
-
-def main():
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--input", help=".cha file folder path")
-    args = vars(ap.parse_args())
-    if args.get("input", None) is not None:
-        # read arguments
-        chaFolderPath = args["input"]
-        for filename in os.listdir(chaFolderPath):
-            match = re.match("[a-z][a-z][0-9][0-9]\.cha",filename)
-            if match:
-                if chaFolderPath[-1] != "/":
-                    writeFileWithAllObject(chaFolderPath + "/" + filename)
-                else:
-                    writeFileWithAllObject(chaFolderPath + filename)
-    #ratioObjectsMatch('/home/lea/Stage/DATA/chaFiles/Rollins/nb09.cha')
-    #perfObjMatch(1)
-    #perfObjMatch(2)
-    #perfObjMatch(3)
-    #sentence = getTextFromCha('/home/lea/Stage/DATA/chaFiles/Rollins/nb09.cha')
-    #print (get_obj_by_POS_and_sentences(sentence))
-        
-    
-
-
-if __name__ == "__main__":
-    main()
